a4231d8f4ab1982a3dadcc338bb302564593c91b
elitemagic <toddmerlin@hotmail.com>
2023-09-13 14:20:42 +1000

    4.5 part 2

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -6,7 +6,7 @@ import NumberOfEvents from "../components/NumberOfEvents";
 
 describe("<NumberOfEvents /> component", () => {
   test("contains an element with role of textbox", () => {
-    const { container } = render(<NumberOfEvents />); // Render the NumberOfEvents component
+    const { container } = render(<NumberOfEvents setCurrentNOE={() => {}} />); // Render the NumberOfEvents component
     const textBox = container.querySelector(
       '[data-testid="numberOfEventsInput"]'
     );
@@ -15,14 +15,14 @@ describe("<NumberOfEvents /> component", () => {
   });
 
   test("default value of input field is 32", () => {
-    const { getByTestId } = render(<NumberOfEvents />);
+    const { getByTestId } = render(<NumberOfEvents setCurrentNOE={() => {}} />);
     const inputElement = getByTestId("numberOfEventsInput");
 
     expect(inputElement).toHaveValue("32");
   });
 
   test("value of textbox changes when user types", async () => {
-    const { getByTestId } = render(<NumberOfEvents />);
+    const { getByTestId } = render(<NumberOfEvents setCurrentNOE={() => {}} />);
     const inputElement = getByTestId("numberOfEventsInput");
 
     await userEvent.type(inputElement, "{backspace}{backspace}10");
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
-
-const NumberOfEvents = () => {
-  const [eventCount, setEventCount] = useState(32);
-
+const NumberOfEvents = ({ setCurrentNOE }) => {
   const handleInputChanged = (event) => {
     const value = event.target.value;
-    setEventCount(value);
+    setCurrentNOE(value);
   };
 
   return (
     <div id="number-of-events">
       <input
         type="text"
-        value={eventCount}
+        defaultValue="32"
         onChange={handleInputChanged}
         data-testid="numberOfEventsInput"
       />
