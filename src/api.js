2da6a6ab43c5a6fc3b4a04c46d8b1f384f573c60
elitemagic <toddmerlin@hotmail.com>
2023-10-03 15:06:26 +1100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -68,6 +68,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -79,6 +85,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
