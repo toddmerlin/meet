bea504ca41abf52e9214834067e59f0302ea0828
elitemagic <toddmerlin@hotmail.com>
2023-10-04 13:25:43 +1100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,16 @@
+// src/api.js
+
 import mockData from "./mock-data";
 import NProgress from "nprogress";
+
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
 
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
